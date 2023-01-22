ed91b191a498e0e0af6f383e1b62f7cd128cac86
ileanahi <argyris.ileana@gmail.com>
2023-01-22 11:28:18 -0600

    Generate Spring Boot project

---
+++ src/main/java/com/capstone/doctorsOffice/DoctorsOfficeApplication.java
@@ -0,0 +1,13 @@
+package com.capstone.doctorsOffice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoctorsOfficeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoctorsOfficeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/capstone/doctorsOffice/DoctorsOfficeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.doctorsOffice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoctorsOfficeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
