c2d497fb903c5f9a335a159cf3b219d0ff9515b2
GRISONRF <grisonrf@gmail.com>
2023-01-27 15:44:35 -0700

    Add Data, AllArgsConstructor, NoArgsConstructor and delete getters and setters

--- src/main/java/com/capstone/doctorsOffice/entities/Patient.java
+++ src/main/java/com/capstone/doctorsOffice/entities/Patient.java
@@ -1,7 +1,14 @@
 package com.capstone.doctorsOffice.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "patients")
 public class Patient {
@@ -19,51 +26,5 @@ public class Patient {
     @OneToOne(mappedBy = "patient")
     private Appointment appointment;
 
-    // constructor, getters, and setters
-    public Patient() {}
 
-    public Patient(String name, String address) {
-        this.name = name;
-        this.address = address;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public Doctor getDoctor() {
-        return doctor;
-    }
-
-    public void setDoctor(Doctor doctor) {
-        this.doctor = doctor;
-    }
-
-    public Appointment getAppointment() {
-        return appointment;
-    }
-
-    public void setAppointment(Appointment appointment) {
-        this.appointment = appointment;
-    }
 }
