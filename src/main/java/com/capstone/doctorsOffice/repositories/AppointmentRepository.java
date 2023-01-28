437a74f764f2645defad93ace92aaba7eb9cf36f
asya-code <77514066+asya-code@users.noreply.github.com>
2023-01-28 15:33:15 -0800

    AppointmentRepository restored

---
+++ src/main/java/com/capstone/doctorsOffice/repositories/AppointmentRepository.java
@@ -0,0 +1,13 @@
+package com.capstone.doctorsOffice.repositories;
+
+import com.capstone.doctorsOffice.entities.Appointment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface AppointmentRepository extends JpaRepository<Appointment, Long>{
+    List<Appointment> findByDoctorId(Long doctorId); //instead of just Long id
+    List<Appointment> findByPatientId(Long patientId);
+}
