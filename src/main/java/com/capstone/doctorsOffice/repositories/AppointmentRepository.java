9065e1849e8d683ef7d993c93fd42e5b85d8aee4
GRISONRF <grisonrf@gmail.com>
2023-02-01 18:40:02 -0700

    delete extra empty lines

--- src/main/java/com/capstone/doctorsOffice/repositories/AppointmentRepository.java
+++ src/main/java/com/capstone/doctorsOffice/repositories/AppointmentRepository.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-
 @Repository
 public interface AppointmentRepository extends JpaRepository<Appointment, Long>{
     List<Appointment> findByDoctorId(Long doctorId); //instead of just Long id
