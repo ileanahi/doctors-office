ec010f29646e8eead3af9729440f56730a59ae07
GRISONRF <grisonrf@gmail.com>
2023-02-01 18:55:56 -0700

    delete imports that I did not need from patientDto

--- src/main/java/com/capstone/doctorsOffice/dtos/PatientDto.java
+++ src/main/java/com/capstone/doctorsOffice/dtos/PatientDto.java
@@ -1,15 +1,11 @@
 package com.capstone.doctorsOffice.dtos;
 
-import com.capstone.doctorsOffice.entities.Doctor;
 import com.capstone.doctorsOffice.entities.Patient;
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
-import java.util.Set;
 
 @Data
 @AllArgsConstructor
