594615c86bbfec98aba6d27d9378d00699235adf
GRISONRF <grisonrf@gmail.com>
2023-02-08 19:50:00 -0700

    patient profile html done

--- src/main/java/com/capstone/doctorsOffice/configuration/SeedDb.java
+++ src/main/java/com/capstone/doctorsOffice/configuration/SeedDb.java
@@ -40,17 +40,18 @@ public class SeedDb implements CommandLineRunner {
         Date parsedTime = format.parse(timeString);
         java.sql.Time time = new java.sql.Time(parsedTime.getTime());
 
-        Doctor doctor1 = new Doctor("Doctor", "Doctor");
+        Doctor doctor1 = new Doctor("Lyndon Mathis", "Cardiologist");
         doctorRespository.save(doctor1);
 
-        Patient patient1 = new Patient(null, "Rafaela", "1@test.com", "123", "123 Street", doctor1,null);
+        Patient patient1 = new Patient(null, "Hamish Moore", "1@test.com", "123", "123 Street, Denver - CO", doctor1,null);
         patientRepository.save(patient1);
 
         Appointment appointment1 = new Appointment(null, date, time, patient1, doctor1);
         appointment1.setDoctor(doctor1);
+        appointment1.setPatient(patient1);
         appointmentRepository.save(appointment1);
 
-        Prescription prescription1 = new Prescription(null, "prescription name", true, date, patient1, doctor1);
+        Prescription prescription1 = new Prescription(null, "Amiodarone - 3mg, 2x day", true, date, patient1, doctor1);
         prescriptionRepository.save(prescription1);
     }
 }
