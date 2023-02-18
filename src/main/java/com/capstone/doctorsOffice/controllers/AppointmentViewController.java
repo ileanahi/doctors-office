48eb3e9a94802c8c16030076e6f2cacd2cce8f0b
asya-code <77514066+asya-code@users.noreply.github.com>
2023-02-18 13:26:24 -0800

    AppointmentViewController added

---
+++ src/main/java/com/capstone/doctorsOffice/controllers/AppointmentViewController.java
@@ -0,0 +1,54 @@
+package com.capstone.doctorsOffice.controllers;
+
+import com.capstone.doctorsOffice.dtos.AppointmentDto;
+import com.capstone.doctorsOffice.dtos.PatientDto;
+import com.capstone.doctorsOffice.entities.Appointment;
+import com.capstone.doctorsOffice.entities.Prescription;
+import com.capstone.doctorsOffice.repositories.AppointmentRepository;
+import com.capstone.doctorsOffice.repositories.PrescriptionRepository;
+import com.capstone.doctorsOffice.services.AppointmentService;
+import com.capstone.doctorsOffice.services.PatientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.crossstore.ChangeSetPersister;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+import java.util.Optional;
+
+
+@ComponentScan
+@Controller
+@RequestMapping("appointmentView")
+public class AppointmentViewController {
+
+    @Autowired
+    private PatientService patientService;
+
+    @Autowired
+    private AppointmentService appointmentService;
+
+    @Autowired
+    private AppointmentRepository appointmentRepository;
+
+   @Autowired
+
+    @GetMapping("/{id}")
+    public String getAppointmentByPatientId(@PathVariable Long id, Model model) throws ChangeSetPersister.NotFoundException {
+       Optional<PatientDto> patientDtoOptional = patientService.getPatientById(id);
+       if (patientDtoOptional.isPresent()) {
+           List<AppointmentDto> appointmentDtoList = appointmentRepository.findByPatientId(id).stream().map(entity -> {
+               return new AppointmentDto(entity);
+           }).toList();
+           Optional<AppointmentDto> appointmentDtoOptional = Optional.ofNullable(appointmentDtoList.get(0));
+           model.addAttribute("appointment", appointmentDtoOptional);
+       }
+
+        return "appointment";
+    }
+
+}
