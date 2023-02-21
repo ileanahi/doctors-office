7109f13be9a3048b2a93aaa2d5f8d99093fab6ec
GRISONRF <grisonrf@gmail.com>
2023-02-20 21:07:48 -0700

    js and html to edit patient's phone number

--- src/main/resources/static/patient/patient-profile.js
+++ src/main/resources/static/patient/patient-profile.js
@@ -12,6 +12,8 @@ const addressEl = document.getElementById('address');
 const editAddressLinkEl = document.getElementById('edit-address-link');
 console.log(patientId)
 console.log(addressEl)
+const phoneEl = document.getElementById('phone');
+console.log(phoneEl.textContent)
 
 
 const patientName = document.getElementById('patientName').textContent;
@@ -22,16 +24,17 @@ console.log(patientName)
 console.log(patientEmail)
 
 
+///////////////////////// EDIT THE ADDRESS ////////////////////////////
 editAddressLinkEl.addEventListener('click', async (event) => {
   event.preventDefault();
   addressEl.contentEditable = true;
   addressEl.focus();
   editAddressLinkEl.innerHTML = "[save]";
-  editAddressLinkEl.addEventListener('click', handleSaveClick);
-  editAddressLinkEl.removeEventListener('click', handleEditClick);
+  editAddressLinkEl.addEventListener('click', handleSaveAddressClick);
+  editAddressLinkEl.removeEventListener('click', handleEditAddressClick);
 });
 
-async function handleSaveClick(event) {
+async function handleSaveAddressClick(event) {
 
   event.preventDefault();
   addressEl.contentEditable = false;
@@ -41,18 +44,19 @@ async function handleSaveClick(event) {
 
     console.log("inside handlesave")
 
-    let bodyUpdate = {
+    let addressUpdate = {
         id: patientId,
         name: patientName,
+        phone: phoneEl.textContent,
         email: patientEmail,
         password: "123",
         address: newAddress,
     }
 
-    console.log(bodyUpdate)
+    console.log(addressUpdate)
     const response = await fetch(`${patientConfig.baseUrl}/${patientId}`, {
         method: "PUT",
-        body: JSON.stringify(bodyUpdate),
+        body: JSON.stringify(addressUpdate),
         headers: patientConfig.headers
     });
 
@@ -67,52 +71,81 @@ async function handleSaveClick(event) {
 
   editAddressLinkEl.innerHTML = "[edit]";
   editAddressLinkEl.removeEventListener('click', arguments.callee);
-  editAddressLinkEl.addEventListener('click', handleEditClick);
+  editAddressLinkEl.addEventListener('click', handleEditAddressClick);
 }
 
 
-async function handleEditClick(event) {
+async function handleEditAddressClick(event) {
     event.preventDefault();
     addressEl.contentEditable = false;
     editAddressLinkEl.innerHTML = "[edit]";
-    editAddressLinkEl.addEventListener('click', handleEditClick);
-    editAddressLinkEl.removeEventListener('click', handleSaveClick);
-  }
+    editAddressLinkEl.addEventListener('click', handleEditAddressClick);
+    editAddressLinkEl.removeEventListener('click', handleSaveAddressClick);
+}
 
 
 
 
 
+////////////////////////// EDIT PHONE NUMBER ///////////////////////////
+const editPhoneLinkEl = document.getElementById('edit-phone-link');
+
+editPhoneLinkEl.addEventListener('click', async (event) => {
+  event.preventDefault();
+  phoneEl.contentEditable = true;
+  phoneEl.focus();
+  editPhoneLinkEl.innerHTML = "[save]";
+  editPhoneLinkEl.addEventListener('click', handleSavePhoneClick);
+  editPhoneLinkEl.removeEventListener('click', handleEditPhoneClick);
+});
+
+async function handleSavePhoneClick(event) {
+
+  event.preventDefault();
+  phoneEl.contentEditable = false;
+
+  const newPhone = phoneEl.textContent;
+  console.log(newPhone)
+
+    console.log("inside handlesave")
+
+    let phoneUpdate = {
+        id: patientId,
+        name: patientName,
+        email: patientEmail,
+        phone: newPhone,
+        password: "123",
+        address: patientAddress,
+    }
+
+    console.log(phoneUpdate)
+    const response = await fetch(`${patientConfig.baseUrl}/${patientId}`, {
+        method: "PUT",
+        body: JSON.stringify(phoneUpdate),
+        headers: patientConfig.headers
+    });
+
+    if (response.ok) {
+        console.log(response)
+        console.log("Patient updated!");
+        window.location.reload();
+    } else {
+        console.error("Failed to update patient.");
+    }
 
 
+  editPhoneLinkEl.innerHTML = "[edit]";
+  editPhoneLinkEl.removeEventListener('click', arguments.callee);
+  editPhoneLinkEl.addEventListener('click', handleEditPhoneClick);
+}
 
 
+async function handleEditPhoneClick(event) {
+    event.preventDefault();
+    phoneEl.contentEditable = false;
+    editPhoneLinkEl.innerHTML = "[edit]";
+    editPhoneLinkEl.addEventListener('click', handleEditPhoneClick);
+    editPhoneLinkEl.removeEventListener('click', handleSavePhoneClick);
+}
 
 
-
-
-
-//addressEl.addEventListener('keydown', (event) => {
-//  if (event.keyCode === 13) { // enter key
-//    event.preventDefault();
-//    addressEl.contentEditable = false;
-
-
-//    const response = await fetch(`${patientConfig.baseUrl}/${patientId}`, {
-//        method: "PUT",
-//        body: JSON.stringify(bodyUpdate),
-//        headers: patientConfig.headers
-//    });
-//
-//
-//    if (response.ok) {
-//        console.log(response)
-//        console.log("Patient updated!");
-//        window.location.reload();
-//    } else {
-//        console.error("Failed to update patient.");
-//    }
-
-
-//  }
-//});
